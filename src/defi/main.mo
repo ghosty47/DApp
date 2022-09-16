a5901b797f8e87d53eed62a3c9d8b01ee4db7ea1
susoroh <susoroh@aortem.io>
2022-09-16 22:14:03 +0100

    first commit on Decentralized app

--- src/defi/main.mo
+++ src/defi/main.mo
@@ -1,5 +1,49 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor Dbank {
+  stable var currentValue: Float = 300;
+  // currentValue := 300;
+
+
+  stable var startTime = Time.now();
+  // startTime := Time.now();
+  Debug.print(debug_show(startTime));
+
+  let id = 234324324343343;
+
+  // Debug.print(debug_show(id));
+
+//private function
+  public func topUp(amount: Float) {
+    // currentValue += amount;
+    Debug.print(debug_show(currentValue));
   };
-};
+
+  // topUp();
+
+  public func witdraw(amount: Float) {
+    let tempValue: Float = currentValue - amount;
+    if (tempValue >= 0) {
+      currentValue -= amount;
+      Debug.print(debug_show(currentValue));
+    } else {
+      Debug.print("Insufficient funds");
+    };
+    
+  };
+
+  //for fast call i.e you are not updating state you just need the value use query
+  public query func checkBalance(): async Float {
+    return currentValue;
+  };
+
+  public func compoundInterest() {
+    let currentTime = Time.now();
+    let timeElaspeNS = currentTime - startTime;
+    let timeElaspeS = timeElaspeNS / 1000000000;
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElaspeS));
+    startTime := currentTime;
+  };
+}
--- src/defi_assets/src/index.js
+++ src/defi_assets/src/index.js
@@ -1,19 +1,38 @@
 import { defi } from "../../declarations/defi";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+window.addEventListener("load", async function() {
+  update();
+});
 
-  const name = document.getElementById("name").value.toString();
+document.querySelector("form").addEventListener("submit", async function(event) {
+  event.preventDefault();
+
+  const button = event.target.querySelector("#submit-btn");
+  
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
+  const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
 
   button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await defi.greet(name);
+  if (document.getElementById("input-amount").value.length != 0) {
+    await defi.topUp(inputAmount);
+  }
+
+  if (document.getElementById("withdrawal-amount").value.length != 0) {
+    await defi.witdraw(outputAmount);
+  }
+
+  await defi.compoundInterest();
+
+  update();
+
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
 
   button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
 });
+
+async function update() {
+  const currentAmount = await defi.checkBalance();
+  document.getElementById("value").innerHTML = Math.round(currentAmount * 100) / 100;
+}
